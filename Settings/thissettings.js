d7e429a9d897007d6bbb3e9c311cfb52dbd11f72
Miex2157 <miex2157@edu.ucl.dk>
2020-05-23 10:22:36 +0200

    lavet JS på pile!

--- Settings/thissettings.js
+++ Settings/thissettings.js
@@ -22,3 +22,65 @@ toolTipElements.forEach(function(elem){
   });
 });
 };
+
+// JS til pile smooth scrool
+
+function scroll_to(first_arrow) {
+      document.documentElement.style.scrollBehavior = 'smooth';
+      element = document.createElement('a');
+      element.setAttribute('href', id);
+      element.click();
+  }
+  document.getElementById("first_arrow").addEventListener('click', () => {
+  scroll_to('#7');
+  });
+
+  function scroll_to(second_arrow) {
+        document.documentElement.style.scrollBehavior = 'smooth';
+        element = document.createElement('a');
+        element.setAttribute('href', id);
+        element.click();
+    }
+    document.getElementById("second_arrow").addEventListener('click', () => {
+    scroll_to('#7');
+    });
+
+    function scroll_to(third_arrow) {
+          document.documentElement.style.scrollBehavior = 'smooth';
+          element = document.createElement('a');
+          element.setAttribute('href', id);
+          element.click();
+      }
+      document.getElementById("third_arrow").addEventListener('click', () => {
+      scroll_to('#7');
+      });
+
+      function scroll_to(fourth_arrow) {
+            document.documentElement.style.scrollBehavior = 'smooth';
+            element = document.createElement('a');
+            element.setAttribute('href', id);
+            element.click();
+        }
+        document.getElementById("fourth_arrow").addEventListener('click', () => {
+        scroll_to('#7');
+        });
+
+        function scroll_to(fif_arrow) {
+              document.documentElement.style.scrollBehavior = 'smooth';
+              element = document.createElement('a');
+              element.setAttribute('href', id);
+              element.click();
+          }
+          document.getElementById("fif_arrow").addEventListener('click', () => {
+          scroll_to('#7');
+          });
+
+          function scroll_to(six_arrow) {
+                document.documentElement.style.scrollBehavior = 'smooth';
+                element = document.createElement('a');
+                element.setAttribute('href', id);
+                element.click();
+            }
+            document.getElementById("six_arrow").addEventListener('click', () => {
+            scroll_to('#7');
+            });
