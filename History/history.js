df2baa2279b107aeaf183fbed7f8721daf6c6bef
emmakabel <emmasecher@gmail.com>
2020-05-25 14:50:29 +0200

    js start

---
+++ History/history.js
@@ -0,0 +1,9 @@
+let listID = ["ID 14", "ID 13", "ID 12", "ID 11", "ID 10", "ID 9", "ID 8", "ID 7", "ID 6", "ID 5", "ID 4", "ID 3", "ID 2", "ID 1"];
+for (let l in listID) {
+    let newElement = document.createElement('div');
+    newElement.id = listID[l];
+    newElement.className = "blabla";
+    newElement.innerHTML = listID[l];
+    document.getElementById('remain').appendchild(newElement);
+
+}
