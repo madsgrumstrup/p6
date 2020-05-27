f63755d6ff581d3ac398796af18ea4d5f79ae91f
Miex2157 <miex2157@edu.ucl.dk>
2020-05-27 15:23:07 +0200

    arrays, for loop, if else done

--- Settings/thissettings.js
+++ Settings/thissettings.js
@@ -39,3 +39,33 @@ function scroll_to(all_arrows) {
 // pensum lavet her: DOM, JS, Events,Operator, Funktioner
 // Jeg mangler if statements, loops og list i form af arrays
 // JS pile smooth scrool done
+
+// array JS til save_exit knap
+
+var colors = ["#25b125" , "#239435" , "black" , "#6B6C6B"];
+function changeColorExit(element, color){
+  element.style.backgroundColor = color;
+}
+// array JS til save_exit knap slut
+
+// greeting if else JS
+function myFunction() {
+  var hour = new Date().getHours();
+  var greeting;
+  if (hour < 16) {
+    greeting = "Good day! The office is still availeble for you";
+  } else {
+    greeting = "Ops! The office is closed for today. See you tomorrow";
+  }
+  document.getElementById("demo").innerHTML = greeting;
+}
+
+// end of greeting if else JS
+
+// for loop
+
+for (i = 0; i < 1; i++) {
+  alert("This settings site is for adding new components")
+}
+
+// for loop slut
