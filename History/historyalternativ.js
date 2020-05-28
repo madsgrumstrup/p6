6f68f0111a61052ec334e8266f1546ce512a2863
emmakabel <emmasecher@gmail.com>
2020-05-28 15:45:47 +0200

    Færdig history alle filer lagt op

--- History/historyalternativ.js
+++ History/historyalternativ.js
@@ -27,8 +27,44 @@ function addExtraBox() {
   let newDate = '01/' + newMonth + '/' + newYear;
   let html = '<div class="hisitem1nt"><div class="hisitemname"><h1>ID ' + count + '</h1></div><div class="hisitemtype"></div></div><div class="hisitem1date"><i class="fas fa-tools"></i><p>' + newDate + '</p></div>';
   let box = document.createElement('div');
-  box.className = "hisitem"
+  box.className = "hisitem";
   box.innerHTML = html;
 
   parent.prepend(box);
 }
+
+
+function forklaring() {
+  let array = [
+      '01/02/19',
+      '01/03/19',
+      '01/04/19',
+      '01/05/19',
+      '01/06/19',
+      '01/07/19',
+      '01/08/19',
+      '01/09/19',
+      '01/10/19',
+      '01/11/19',
+      '01/12/19',
+      '01/01/20',
+      '01/02/20',
+      '01/03/20',
+  ];
+
+  array.forEach((date, index) => {
+    let parent = document.getElementById('parentBox');
+    let box = document.createElement('div');
+    let id = index + 1;
+    let html = '<div class="hisitem1nt"><div class="hisitemname"><h1>ID ' + id +'</h1></div><div class="hisitemtype"></div></div><div class="hisitem1date"><i class="fas fa-tools"></i><p>' + date + '</p></div>';
+    box.className = "hisitem";
+    box.innerHTML = html;
+    parent.prepend(box);
+  });
+
+  /* document.getElementById('knap').addEventListener('click', function () {
+    addExtraBox();
+  });*/
+}
+
+forklaring();
